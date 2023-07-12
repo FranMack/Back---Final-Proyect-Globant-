825dbb4168fb0a0b3d41cde4f0b5fd43c47b05df
cantarodev <jhon.cantaro@unas.edu.pe>
2023-07-12 02:02:36 +0100

    Config database mongodb and run server

---
+++ models/user.model.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const modelOptions = require("./model.options");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    url_img: {
+      type: String,
+      required: false,
+    },
+    phone_number: {
+      type: String,
+      required: false,
+    },
+    ubication: {
+      type: String,
+      requerid: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    is_admin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    salt: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  { modelOptions }
+);
+
+userSchema.methods.setPassword = async function (password) {
+  const salt = bcrypt.genSaltSync(8);
+  this.salt = salt;
+
+  this.password = await bcrypt.hash(password, this.salt);
+};
+
+userSchema.methods.validPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const models = require("./models");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors({ origin: process.env.FRONT_API_URL, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
