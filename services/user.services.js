72f3ddb0fc83e5995ce61174e09aa98da720b5eb
v4leng <valem.g_13@hotmail.com>
2023-07-12 14:45:29 -0300

    users-route

--- services/user.services.js
+++ services/user.services.js
@@ -1,6 +1,5 @@
 const userModel = require("../models/user.model")
 
-
 class UserService {
   async createUser(data) {
     try {
@@ -17,6 +16,50 @@ class UserService {
       throw new Error("Error creating user: " + error.message);
     }
   }
+  static async getProfile(username) {
+    try {
+      const userProfile = await userModel.find({ username: username });
+
+      if (userProfile.length === 0) {
+        throw new Error("Usuario no encontrado");
+      }
+
+      return userProfile;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async updateProfile(username, dataUser) {
+    try {
+      const user = await userModel.findOne({ username: username });
+      const {
+        email,
+        phone_number,
+        url_img,
+        ubication,
+        is_admin,
+        first_name,
+        last_name,
+      } = dataUser;
+
+      if (!user) {
+        throw new Error("Usuario no encontrado");
+      }
+
+      user.email = email;
+      user.first_name = first_name;
+      user.last_name = last_name;
+      user.phone_number = phone_number;
+      user.url_img = url_img;
+      user.ubication = ubication;
+      user.is_admin = is_admin || false;
+      return user.save();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
 }
 
 module.exports = UserService;
