e6f7661e170f7ebc98237859504c9524bd97bd48
vik1nga <victoria.fonteviva@gmail.com>
2023-07-19 15:50:46 -0300

    Reports and Offices Routes creation

---
+++ controllers/report.controllers.js
@@ -0,0 +1,64 @@
+const ReportService = require("../services/report.services");
+
+class ReportController {
+  static async createReport(req, res) {
+    try {
+      const reportData = req.body;
+      if (Object.keys(reportData).length === 0) {
+        return res.status(400).json({ error: "Report data is empty." });
+      }
+      const createdReport = await ReportService.createReport(reportData);
+      res.status(201).json(createdReport);
+      console.log(createdReport);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create report." });
+    }
+  }
+
+  static async editReport(req, res) {
+    try {
+      let reportId = req.params.reportId.trim();
+      const reportData = req.body;
+      const updatedReport = await ReportService.editReport(
+        reportId,
+        reportData
+      );
+      res.status(200).json(updatedReport);
+    } catch (error) {
+      console.error("Error in editReport:", error);
+      res.status(500).json({ error: "Failed to edit report controllers" });
+    }
+  }
+  static async deleteReport(req, res) {
+    try {
+      let { id } = req.params;
+      const result = await ReportService.deleteReport(id);
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).send("Failed to delete report in controller.");
+    }
+  }
+
+  static async getAllReports(req, res) {
+    try {
+      const allReports = await ReportService.getAllReports();
+      res.status(200).json(allReports);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch all reports." });
+    }
+  }
+
+  static async getAllReportsByStatus(req, res) {
+    try {
+      const statusReport = req.params.statusReport;
+      const reportsByStatus = await ReportService.getAllReportsByStatus(
+        statusReport
+      );
+      res.status(200).json(reportsByStatus);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch reports by status." });
+    }
+  }
+}
+
+module.exports = ReportController;
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const models = require("./models");
 require("dotenv").config();
 const routes = require("./routes/index");
 const app = express();
 
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +16,10 @@ app.use(cookieParser());
 app.use("/api/v1", routes);
 
 const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 mongoose
   .connect(process.env.MONGODB_URL, {
