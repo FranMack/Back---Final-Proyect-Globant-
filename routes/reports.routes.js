8675c2e50139b435a85bc9c0158f2f13dfa66b3e
FranMack <franmack556@gmail.com>
2023-07-25 10:34:41 -0300

    single report route

--- routes/reports.routes.js
+++ routes/reports.routes.js
@@ -7,6 +7,7 @@ routerReport.post("/newReport", ReportController.createReport);
 routerReport.put("/editReport/:reportId", ReportController.editReport);
 routerReport.delete("/delete/:id", ReportController.deleteReport);
 routerReport.get("/all", ReportController.getAllReports);
+routerReport.get("/:id", ReportController.getReportById);
 routerReport.get(
   "/status/:statusReport",
   ReportController.getAllReportsByStatus
