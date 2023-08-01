cdca456e217b8fa17eac5fea7ab5cd1c4d0ff63e
FranMack <franmack556@gmail.com>
2023-08-01 19:45:44 -0300

    Admin search routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,7 +2,54 @@ const express = require("express");
 const UserAdmincontroller = require("../controllers/userAdmin.controllers");
 const routerAdmin = express.Router();
 
-routerAdmin.get("/users-list", UserAdmincontroller.listUsers())
+routerAdmin.get("/users-list", UserAdmincontroller.listUsers)
+
+
+
+
+
+const Users = require("../models/user.model");
+
+//provisoriamente
+
+
+
+routerAdmin.get("/search", (req, res) => {
+  
+
+  const searchValue = req.query.searchValue;
+  const regex = new RegExp(searchValue, "i");
+
+  Users.find({
+    $or: [
+      { email: regex },
+      { first_name: regex },
+      { last_name: regex },
+      { username: regex }
+    ]
+  })
+    .then((users) => res.json(users))
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
+routerAdmin.get("/search-location", (req, res) => {
+  
+
+    const searchValue = req.query.searchValue;
+    const regex = new RegExp(searchValue, "i");
+  
+    Users.find({
+     location:regex
+    })
+      .then((users) => res.json(users))
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
 
 
 module.exports = routerAdmin;
