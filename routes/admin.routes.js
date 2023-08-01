2c4692d1a0b472d7c54980b2508782e587ef3c17
v4leng <valem.g_13@hotmail.com>
2023-08-01 13:08:31 -0300

    index-router-admin

---
+++ routes/admin.routes.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const routerAdmin = express.Router();
+
+
+
+
+module.exports = routerAdmin;
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const routerUser = require("./user.routes");
 const routerReport = require("./reports.routes");
 const routerOffice = require("./office.routes");
+const routerAdmin = require("./admin.routes")
 
 router.use("/user", routerUser);
 router.use("/report", routerReport);
 router.use("/office", routerOffice);
+router.use("/userAdmin", routerAdmin )
 
 module.exports = router;
