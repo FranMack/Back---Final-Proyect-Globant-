71edcf0edbffe88a7113981f62bb695a8b9f0adf
vik1nga <victoria.fonteviva@gmail.com>
2023-08-02 17:27:29 -0300

    admin adjustments

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const UserAdmincontroller = require("../controllers/userAdmin.controllers");
+const ReportController = require("../controllers/report.controllers");
 const routerAdmin = express.Router();
 
-routerAdmin.get("/users-list", UserAdmincontroller.listUsers)
-routerAdmin.get("/search",UserAdmincontroller.searchUsers)
-routerAdmin.get("/search-location",UserAdmincontroller.filterUsersByLocation)
+routerAdmin.get("/users-list", UserAdmincontroller.listUsers);
+routerAdmin.get("/search", UserAdmincontroller.searchUsers);
+routerAdmin.get("/search-location", UserAdmincontroller.filterUsersByLocation);
+routerAdmin.delete("/report/delete/:id", ReportController.deleteReport);
 
 module.exports = routerAdmin;
-
-
-
-
