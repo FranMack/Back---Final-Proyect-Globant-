b6ca0a9a9f8845382400e1ca105fe52d5157ba24
Micaela <micaelauvilla9@gmail.com>
2023-08-02 16:42:10 -0300

    add selectDesk to admin

--- controllers/userAdmin.controllers.js
+++ controllers/userAdmin.controllers.js
@@ -1,46 +1,54 @@
 const UserAdminServices = require("../services/userAdmin.services");
 
 class UserAdmincontroller {
-    static async listUsers(req,res) {
-        try{
-          const userList = await UserAdminServices.listUsers();
-          res.status(200).json(userList)
-        }
-        catch (error) {
-            res.status(500).json({ error: "Failed to fetch all users." });
-          }
+  static async listUsers(req, res) {
+    try {
+      const userList = await UserAdminServices.listUsers();
+      res.status(200).json(userList);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch all users." });
     }
+  }
 
-    static async searchUsers(req,res){
-
-      const searchValue = req.query.searchValue;
-      try{
-
-        const users=await UserAdminServices.searchUsers(searchValue)
-        res.json(users);
-
-      }
-
-      catch (error) {
-        res.status(500).json({ error: "Failed to fetch all users." });
-      }
+  static async searchUsers(req, res) {
+    const searchValue = req.query.searchValue;
+    try {
+      const users = await UserAdminServices.searchUsers(searchValue);
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch all users." });
     }
+  }
 
-
-    static async filterUsersByLocation(req,res){
-
-      const searchValue = req.query.searchValue;
-      try{
-
-        const users=await UserAdminServices.filterUsersByLocation(searchValue)
-        res.json(users);
-
-      }
-
-      catch (error) {
-        res.status(500).json({ error: "Failed to fetch all users." });
-      }
+  static async filterUsersByLocation(req, res) {
+    const searchValue = req.query.searchValue;
+    try {
+      const users = await UserAdminServices.filterUsersByLocation(searchValue);
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch all users." });
     }
+  }
+  static async getUserReports(req, res) {
+    try {
+      const { user } = req.params;
+      const allReports = await UserAdminServices.getUserReports(user);
+      res.status(200).json(allReports);
+    } catch (error) {
+      res.status(400).json({ error: "Failed to fetch all reports " });
+    }
+  }
+  static async selectDesk(req, res) {
+    try {
+      const { officeId, deskNumber } = req.body;
+
+      const message = await UserAdminServices.selectDesk(officeId, deskNumber);
+
+      return res.status(200).json({ message });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 }
 
 module.exports = UserAdmincontroller;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,17 +1,12 @@
 const express = require("express");
 
-const AdminController = require("../controllers/admin.controllers");
 const UserAdmincontroller = require("../controllers/userAdmin.controllers");
 const routerAdmin = express.Router();
 
-routerAdmin.get("/users-list", UserAdmincontroller.listUsers)
-routerAdmin.get("/search",UserAdmincontroller.searchUsers)
-routerAdmin.get("/search-location",UserAdmincontroller.filterUsersByLocation)
-routerAdmin.get("/reports/:user", AdminController.getUserReports);
+routerAdmin.get("/users-list", UserAdmincontroller.listUsers);
+routerAdmin.get("/search", UserAdmincontroller.searchUsers);
+routerAdmin.get("/search-location", UserAdmincontroller.filterUsersByLocation);
+routerAdmin.get("/reports/:user", UserAdmincontroller.getUserReports);
+routerAdmin.put("/select-desk", UserAdmincontroller.selectDesk);
 
 module.exports = routerAdmin;
-
-
-
-
-
