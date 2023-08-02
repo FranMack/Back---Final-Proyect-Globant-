7a0fce70e13580c25e42870956db3a799bc9e33d
FranMack <franmack556@gmail.com>
2023-08-02 09:17:41 -0300

    Admin search routes

--- controllers/userAdmin.controllers.js
+++ controllers/userAdmin.controllers.js
@@ -10,6 +10,37 @@ class UserAdmincontroller {
             res.status(500).json({ error: "Failed to fetch all users." });
           }
     }
+
+    static async searchUsers(req,res){
+
+      const searchValue = req.query.searchValue;
+      try{
+
+        const users=await UserAdminServices.searchUsers(searchValue)
+        res.json(users);
+
+      }
+
+      catch (error) {
+        res.status(500).json({ error: "Failed to fetch all users." });
+      }
+    }
+
+
+    static async filterUsersByLocation(req,res){
+
+      const searchValue = req.query.searchValue;
+      try{
+
+        const users=await UserAdminServices.filterUsersByLocation(searchValue)
+        res.json(users);
+
+      }
+
+      catch (error) {
+        res.status(500).json({ error: "Failed to fetch all users." });
+      }
+    }
 }
 
 module.exports = UserAdmincontroller;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,53 +3,11 @@ const UserAdmincontroller = require("../controllers/userAdmin.controllers");
 const routerAdmin = express.Router();
 
 routerAdmin.get("/users-list", UserAdmincontroller.listUsers)
-
-
-
-
-
-const Users = require("../models/user.model");
-
-//provisoriamente
-
-
-
-routerAdmin.get("/search", (req, res) => {
-  
-
-  const searchValue = req.query.searchValue;
-  const regex = new RegExp(searchValue, "i");
-
-  Users.find({
-    $or: [
-      { email: regex },
-      { first_name: regex },
-      { last_name: regex },
-      { username: regex }
-    ]
-  })
-    .then((users) => res.json(users))
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-
-routerAdmin.get("/search-location", (req, res) => {
-  
-
-    const searchValue = req.query.searchValue;
-    const regex = new RegExp(searchValue, "i");
-  
-    Users.find({
-     location:regex
-    })
-      .then((users) => res.json(users))
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-
-
+routerAdmin.get("/search",UserAdmincontroller.searchUsers)
+routerAdmin.get("/search-location",UserAdmincontroller.filterUsersByLocation)
 
 module.exports = routerAdmin;
+
+
+
+
